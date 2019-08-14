e6ea62c55fec3682a30dd7afcfe441fc453ced83
hakanErgin <hakan.ergin@gmail.com>
2019-08-14 17:33:43 +0200

    wip

--- project/script.js
+++ project/script.js
@@ -1,28 +1,33 @@
 const countries = function () {
 
-
-  fetch('https://restcountries.eu/rest/v2/all')
+  fetch("https://restcountries.eu/rest/v2/all")
     .then(function (data1) {
-      console.log(data1)
+      // console.log(data1)
       return data1.json()
     })
     .then(function (data) {
-      console.log(data)
+      // console.log(data)
+      const listElem = document.createElement("select"); // create list
+      document.querySelector("#div2").appendChild(listElem);
+      listElem.id = "dropdownList";
 
-      const list = document.createElement("select");
-      document.body.appendChild(list);
-      list.id = "dropdownList";
-
-      const makeSelectOptions = data.map(function (x) {
-        const selectOption = document.createElement("option")
-        selectOption.innerHTML = x.name //fills the html
-        selectOption.value = x.name //fills value
-        list.appendChild(selectOption)
-        
+      //  map data and populate list
+      data.map(function (x) {
+        this.optionElem = document.createElement("option")
+        optionElem.innerHTML = x.name //  fills html list
+        optionElem.value = x.name //  fills value
+        listElem.appendChild(optionElem) // adds to list
+        return x.name
       })
-      
+
+      console.log(listElem);
+
+      //  select/change
+      listElem.addEventListener("change", (event) => {
+        const selection = document.querySelector("#div3");
+        selection.textContent = `${event.target.value}`;
+      });
     });
 }
-
 
 document.getElementById("btn").addEventListener("click", countries);
