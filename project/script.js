6c72ae0722efccc8793c2f8d069c64e5d4f52985
hakanErgin <hakan.ergin@gmail.com>
2019-08-17 11:32:57 +0200

    could not finish

--- project/script.js
+++ project/script.js
@@ -1,21 +1,21 @@
 const countries = function () {
-
+    
   fetch("https://restcountries.eu/rest/v2/all")
     .then(function (data1) {
       // console.log(data1)
       return data1.json()
     })
     .then(function (data) {
-      // console.log(data)
+      console.log(data)
       const listElem = document.createElement("select"); // create list
-      document.querySelector("#div2").appendChild(listElem);
+      document.querySelector("#div2").appendChild(listElem)
       listElem.id = "dropdownList";
 
       //  map data and populate list
       data.map(function (x) {
-        this.optionElem = document.createElement("option")
+        optionElem = document.createElement("option")
         optionElem.innerHTML = x.name //  fills html list
-        optionElem.value = x.name //  fills value
+        // optionElem.value = x.name //  fills value
         listElem.appendChild(optionElem) // adds to list
         return x.name
       })
@@ -24,10 +24,24 @@ const countries = function () {
 
       //  select/change
       listElem.addEventListener("change", (event) => {
-        const selection = document.querySelector("#div3");
-        selection.textContent = `${event.target.value}`;
-      });
-    });
+        const selection = document.querySelector("#div3")
+        selection.textContent = event.target.value
+        const selContent = selection.textContent
+        console.log(selContent);
+
+
+        const selectedCountry = data.filter(country => country.name === selContent)
+        console.log(selectedCountry);
+
+        //  creates content for selected
+        function createContent(a) {
+          console.log(a);
+        };
+
+        createContent(selContent);
+      })
+    })
+  
 }
 
 document.getElementById("btn").addEventListener("click", countries);
